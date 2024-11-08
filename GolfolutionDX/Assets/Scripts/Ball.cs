e711a9f416a695be7baf28ec1159a6bec8cad0f1
Matt McCollum <mmccollum31@gatech.edu>
2024-11-07 20:18:32 -0500

    Add basic kicking script to Ball

---
+++ GolfolutionDX/Assets/Scripts/Ball.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour {
+    private Rigidbody rb;
+    public float speed = 10.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // apply kicking forces when space bar is pressed
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.AddForce(Vector3.forward * speed, ForceMode.Impulse);
+            rb.AddForce(Vector3.up * speed, ForceMode.Impulse);
+        }
+
+    }
+}
