d7c7032aa02cd42ef79b54057e6b55ad2041af92
Matt McCollum <mmccollum31@gatech.edu>
2024-11-07 20:43:31 -0500

    Add physics material for Ball

--- GolfolutionDX/Assets/Scripts/Ball.cs
+++ GolfolutionDX/Assets/Scripts/Ball.cs
@@ -4,12 +4,14 @@ using UnityEngine;
 
 public class Ball : MonoBehaviour {
     private Rigidbody rb;
+    //private SphereCollider sc;
     public float speed = 10.0f;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        //sc = GetComponent<SphereCollider>();
     }
 
     // Update is called once per frame
@@ -18,6 +20,7 @@ public class Ball : MonoBehaviour {
         // apply kicking forces when space bar is pressed
         if (Input.GetKeyDown(KeyCode.Space))
         {
+            
             rb.AddForce(Vector3.forward * speed, ForceMode.Impulse);
             rb.AddForce(Vector3.up * speed, ForceMode.Impulse);
         }
