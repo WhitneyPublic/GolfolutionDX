b104bfc85f40120b7c3adb3494b61904304fb0ce
Matt McCollum <mmccollum31@gatech.edu>
2024-11-12 12:52:38 -0500

    Update Ball.cs

--- GolfolutionDX/Assets/Scripts/Ball and Player/Ball.cs
+++ GolfolutionDX/Assets/Scripts/Ball and Player/Ball.cs
@@ -6,6 +6,7 @@ public class Ball : MonoBehaviour {
     private Rigidbody rb;
     //private SphereCollider sc;
     public float speed = 10.0f;
+    public float stopThreshold = 0.2f;
 
     // Start is called before the first frame update
     void Start()
@@ -23,6 +24,18 @@ public class Ball : MonoBehaviour {
         {
             rb.AddForce(Vector3.forward * speed, ForceMode.Impulse);
             rb.AddForce(Vector3.up * speed, ForceMode.Impulse);
+            // apply a random rotational force to the ball
+            Vector3 randSpin = Random.insideUnitSphere * speed * 2;
+            rb.AddTorque(randSpin);
+        }
+        // stop the ball if it is moving slowly
+        if (rb.velocity.magnitude > 0.0f) { // ball is moving
+            Vector2 ballHorizontal = new Vector2(rb.velocity.x, rb.velocity.z);
+            if (ballHorizontal.magnitude < stopThreshold) {
+                rb.velocity = new Vector3(0.0f, rb.velocity.y, 0.0f);
+                rb.freezeRotation = true;
+                rb.freezeRotation = false;
+            }
         }
 
     }
